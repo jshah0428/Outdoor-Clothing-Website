825c386d38b025ec4b20dcfeae6f466100e3e0f3
Jainam Shah <jainamshah0428@gmail.com>
2024-02-16 01:43:12 -0500

    commit header footer

--- footer.php
+++ footer.php
@@ -1,3 +1,12 @@
+
+<?php
+#Jainam Shah
+#2/16/24
+#IT202 - SECTION 006
+#Phase 1 - website
+#jds227@njit.edu
+?>
+
 <html>
 <footer>
     <p> <em>&copy; Jainam Shah. All rights reserved. Contact <a href= "mailto:outdoorventures@gmail.com" > outdoorventures@gmail.com </a> for questions </em></p>
